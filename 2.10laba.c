3065c95a6e1de71b38e31f286ceae98966501600
victoria <statzenko.victorija@yandex.ru>
2024-10-28 21:59:18 +0300

    Сохранение изменений

--- 2.10laba.c
+++ 2.10laba.c
@@ -47,11 +47,36 @@ void polynomial_decomposition(double epsilon, double a, double** g_coeffs, int n
     free(f_coeffs);
 }
 
+double f(double x, int n, ...) {
+    va_list args;
+    va_start(args, n);
+    double result = 0.0;
+
+    for (int i = 0; i <= n; i++) {
+        double coeff = va_arg(args, double);
+        result += coeff * pow(x, i);
+    }
+    va_end(args);
+    return result;
+}
+
+double g(double* coeffs, int n, double a, double x) {
+    double result = 0.0;
+    double x_a = x - a; 
+
+    for (int i = 0; i <= n; i++) {
+        result += coeffs[i] * pow(x_a, i);
+    }
+
+    return result;
+}
+
 int main() {
     double* g_coeffs;
     double a = 2.0; 
     double epsilon = 0.0001; 
     int n = 3; 
+    double x = 1.0;
 
     polynomial_decomposition(epsilon, a, &g_coeffs, n, 1.0, 2.0, 3.0, 4.0);
 
@@ -59,6 +84,12 @@ int main() {
     for (int i = 0; i <= n; i++) {
         printf("g[%d] = %.5f\n", i, g_coeffs[i]);
     }
+    printf("Проверка:\n");
+    double f_value = f(x, n, 1.0, 2.0, 3.0, 4.0);
+    double g_value = g(g_coeffs, n, a, x);
+
+    printf("f(%.2f) = %.5f\n", x, f_value);
+    printf("g(%.2f) = %.5f\n", x, g_value);
 
     free(g_coeffs);
     return 0;
